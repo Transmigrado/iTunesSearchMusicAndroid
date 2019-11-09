f310f1bf7b92744319551d2eb642dfdde9e606a3
transmigrado <transmigrado@gmail.com>
2019-11-09 12:41:15 -0300

    implement api fetch data for viewmodel

---
+++ app/src/main/java/com/blueprint/itunes/datasource/api/RestTrack.kt
@@ -0,0 +1,32 @@
+package com.blueprint.itunes.datasource.api
+
+import com.blueprint.itunes.model.Track
+import com.blueprint.itunes.model.TrackSearchResponse
+import retrofit.Callback
+import retrofit.RetrofitError
+import retrofit.client.Response
+
+class RestTrack {
+    private var servicesApiInterface: ApiClient.ServicesApiInterface
+
+    init {
+        servicesApiInterface = ApiClient.getApiClient()
+    }
+
+    fun fetch(author:String, callback: CallBack){
+        servicesApiInterface.tracks(author,object : Callback<TrackSearchResponse> {
+            override fun success(trackResponse: TrackSearchResponse?, response: Response) {
+                callback.response(trackResponse!!.results)
+            }
+
+            override fun failure(error: RetrofitError?) {
+
+            }
+        })
+    }
+
+
+    interface CallBack {
+        fun response(tracks: ArrayList<Track>)
+    }
+}
--- app/src/main/java/com/blueprint/itunes/viewmodel/TracksModel.kt
+++ app/src/main/java/com/blueprint/itunes/viewmodel/TracksModel.kt
@@ -1,7 +1,31 @@
 package com.blueprint.itunes.viewmodel
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.LiveData
+import com.blueprint.itunes.model.Track
+
 
 class TracksModel : ViewModel(){
-    
+
+    private var tracks: MutableLiveData<List<Track>>? = null
+
+
+    fun getTracks(): LiveData<List<Track>> {
+
+        if (tracks == null) {
+            tracks = MutableLiveData<List<Track>>()
+            loadTracks()
+        }
+
+
+        return tracks as MutableLiveData<List<Track>>
+    }
+
+
+
+    private fun loadTracks() {
+
+
+    }
 }
