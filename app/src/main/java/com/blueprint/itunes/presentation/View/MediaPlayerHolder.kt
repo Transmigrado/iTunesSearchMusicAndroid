3649c120c86639f7dc2544d40e3f90d569cdbaf2
transmigrado <transmigrado@gmail.com>
2019-11-09 15:23:36 -0300

    implement media player holder

---
+++ app/src/main/java/com/blueprint/itunes/presentation/View/MediaPlayerHolder.kt
@@ -0,0 +1,34 @@
+package com.blueprint.itunes.presentation.View
+
+import android.content.Context
+import android.media.AudioManager
+import android.media.MediaPlayer
+import android.net.Uri
+
+class MediaPlayerHolder(ctx:Context, url:String) {
+
+    var mediaPlayer : MediaPlayer
+
+    init {
+        val myUri  = Uri.parse(url)
+
+        mediaPlayer = MediaPlayer().apply {
+            setDataSource(ctx, myUri)
+            prepare()
+        }
+    }
+
+    fun play(){
+        if(!mediaPlayer.isPlaying()){
+            mediaPlayer.start()
+        }
+    }
+
+    fun pause(){
+        if(mediaPlayer.isPlaying()){
+            mediaPlayer.pause()
+        }
+    }
+
+
+}
--- app/src/main/java/com/blueprint/itunes/presentation/activity/DetailTrack.kt
+++ app/src/main/java/com/blueprint/itunes/presentation/activity/DetailTrack.kt
@@ -3,6 +3,7 @@ package com.blueprint.itunes.presentation.activity
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.blueprint.itunes.R
+import com.blueprint.itunes.presentation.View.MediaPlayerHolder
 import kotlinx.android.synthetic.main.detail_track.*
 
 class DetailTrack : AppCompatActivity() {
@@ -10,6 +11,9 @@ class DetailTrack : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.detail_track)
+
+        val playerHolder = MediaPlayerHolder(this,"https://audio-ssl.itunes.apple.com/itunes-assets/Music5/v4/b5/92/77/b592775e-52e8-94ed-daf7-13a3f8bfc550/mzaf_7288708462612507242.plus.aac.p.m4a")
+        playerHolder.play()
 
     }
 }
