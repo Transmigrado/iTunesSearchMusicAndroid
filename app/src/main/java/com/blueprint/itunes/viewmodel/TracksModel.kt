f23b51d88888aa3c16dd6cf4504f135ba51b91eb
transmigrado <transmigrado@gmail.com>
2019-11-09 12:45:55 -0300

    implement Tracks Model

--- app/src/main/java/com/blueprint/itunes/viewmodel/TracksModel.kt
+++ app/src/main/java/com/blueprint/itunes/viewmodel/TracksModel.kt
@@ -3,29 +3,36 @@ package com.blueprint.itunes.viewmodel
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.LiveData
+import com.blueprint.itunes.datasource.api.RestTrack
 import com.blueprint.itunes.model.Track
 
 
 class TracksModel : ViewModel(){
 
-    private var tracks: MutableLiveData<List<Track>>? = null
+    private var tracks: MutableLiveData<ArrayList<Track>>? = null
 
 
-    fun getTracks(): LiveData<List<Track>> {
+    fun getTracks(): LiveData<ArrayList<Track>> {
 
         if (tracks == null) {
-            tracks = MutableLiveData<List<Track>>()
+            tracks = MutableLiveData()
             loadTracks()
         }
 
 
-        return tracks as MutableLiveData<List<Track>>
+        return tracks as MutableLiveData<ArrayList<Track>>
     }
 
 
 
     private fun loadTracks() {
 
+        val rest = RestTrack()
+        rest.fetch("foals", object : RestTrack.CallBack {
+            override fun response(tracks: ArrayList<Track>) {
+                this@TracksModel.tracks!!.value = tracks
+            }
+        })
 
     }
 }
