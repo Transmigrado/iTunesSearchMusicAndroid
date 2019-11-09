e2b1e8c5a09bd71008fbf083045e235e7d46f081
transmigrado <transmigrado@gmail.com>
2019-11-09 12:52:43 -0300

    add track adapter

---
+++ app/src/main/java/com/blueprint/itunes/presentation/adapter/TrackAdapter.kt
@@ -0,0 +1,25 @@
+package com.blueprint.itunes.presentation.adapter
+
+import android.content.Context
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.blueprint.itunes.model.Track
+import com.blueprint.itunes.presentation.holder.TrackHolder
+
+class ClassAdapter(ctx: Context, tracks: ArrayList<Track>) : RecyclerView.Adapter<TrackHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TrackHolder {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getItemCount(): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onBindViewHolder(holder: TrackHolder, position: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    init {
+
+    }
+}
---
+++ app/src/main/java/com/blueprint/itunes/presentation/holder/TrackHolder.kt
@@ -0,0 +1,8 @@
+package com.blueprint.itunes.presentation.holder
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+class TrackHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+}
